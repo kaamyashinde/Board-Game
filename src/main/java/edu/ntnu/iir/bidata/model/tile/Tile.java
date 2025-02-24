2ae9b20b7fdba0c400627ee9eb79e4607f586e26
Durva <durvaparmar19@gmail.com>
2025-02-24 20:33:45 +0100

    Implemented tile class

---
+++ src/main/java/edu/ntnu/iir/bidata/model/tile/Tile.java
@@ -0,0 +1,56 @@
+package edu.ntnu.iir.bidata.model.tile;
+import edu.ntnu.iir.bidata.model.Player;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Represents a tile on the game board.
+ * @author Durva
+ * @version 0.0.1
+ */
+@Getter
+@Setter
+public class Tile {
+  private int id;
+  private TileAction action;
+
+  /**
+   * -- SETTER --
+   *  Sets the next tile in the sequence.
+   *
+   * @param nextTile The next tile to set.
+   */
+  @Setter
+  private Tile nextTile;
+
+  /**
+   * Executes the action associated with this tile
+   *
+   * @param player The player who landed on this tile.
+   */
+
+  public void performAction(Player player) {
+    if (action != null) {
+      action.performAction(player);
+    }
+  }
+
+  /**
+   * Places the player on this tile.
+   *
+   * @param player The player to place on this tile.
+   */
+  public void landPlayer(Player player) {
+    player.placeOnTile(this);
+    performAction(player);
+  }
+
+  /**
+   * Removes the player from this tile.
+   *
+   * @param player The player to remove from this tile.
+   */
+  public void leavePlayer(Player player) {
+    // need more?
+  }
+}
