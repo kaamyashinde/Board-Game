cdb53ff1b82fac548975964a157421b6f9953491
kaamyashinde <kaamyashinde@gmail.com>
2025-02-25 15:56:17 +0100

    import player class in interface

--- src/main/java/edu/ntnu/iir/bidata/model/tile/TileAction.java
+++ src/main/java/edu/ntnu/iir/bidata/model/tile/TileAction.java
@@ -1,5 +1,7 @@
 package edu.ntnu.iir.bidata.model.tile;
 
+import edu.ntnu.iir.bidata.model.Player;
+
 public interface TileAction {
   void performAction (Player player);
   String getDescription();
