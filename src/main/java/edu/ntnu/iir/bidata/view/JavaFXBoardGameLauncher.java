bf2d329f0ba196d289b53dfc1413fea2ba69ff21
Durva <durvaparmar19@gmail.com>
2025-05-05 15:18:37 +0200

    update boardgamelauncher javafx

--- src/main/java/edu/ntnu/iir/bidata/view/JavaFXBoardGameLauncher.java
+++ src/main/java/edu/ntnu/iir/bidata/view/JavaFXBoardGameLauncher.java
@@ -1,8 +1,5 @@
 package edu.ntnu.iir.bidata.view;
 
-import edu.ntnu.iir.bidata.controller.GameController;
-import edu.ntnu.iir.bidata.model.BoardGame;
-import edu.ntnu.iir.bidata.model.Player;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -12,29 +9,19 @@ import javafx.stage.Stage;
 public class JavaFXBoardGameLauncher extends Application {
     @Override
     public void start(Stage primaryStage) {
-        try {
-            // Create a new game with 1 dice and 25 tiles
-            BoardGame boardGame = new BoardGame(1, 25);
-            
-            // Add default players
-            boardGame.addPlayer(new Player("Player 1"));
-            boardGame.addPlayer(new Player("Player 2"));
-            
-            // Initialize the game
-            boardGame.initialiseGame();
-            
-            // Create the JavaFX UI
-            JavaFXGameUI gameUI = new JavaFXGameUI(boardGame);
-            
-            // Create the game controller
-            GameController gameController = new GameController(boardGame, gameUI);
-            
-            // Start the game
-            gameController.startGame();
-        } catch (Exception e) {
-            System.err.println("An error occurred during the game: " + e.getMessage());
-            e.printStackTrace();
-        }
+        showMainMenu(primaryStage);
+    }
+
+    private void showMainMenu(Stage stage) {
+        new MainMenuUI(stage, () -> showSnakesAndLaddersMenu(stage));
+    }
+
+    private void showSnakesAndLaddersMenu(Stage stage) {
+        new SnakesAndLaddersMenuUI(stage, () -> showGameBoard(stage));
+    }
+
+    private void showGameBoard(Stage stage) {
+        new SnakesAndLaddersGameUI(stage);
     }
 
     public static void main(String[] args) {
