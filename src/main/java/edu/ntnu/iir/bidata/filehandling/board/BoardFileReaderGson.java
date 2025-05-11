6a0bb248c2398656e87f8e943b68bbd7eb2e09ee
Kaamya Shinde <kaamyashinde@gmail.com>
2025-05-11 14:27:17 +0200

    chore: create board gson file handlers

---
+++ src/main/java/edu/ntnu/iir/bidata/filehandling/board/BoardFileReaderGson.java
@@ -0,0 +1,12 @@
+package edu.ntnu.iir.bidata.filehandling.board;
+
+import edu.ntnu.iir.bidata.model.board.Board;
+import java.nio.file.Path;
+
+public class BoardFileReaderGson implements BoardFileReader {
+    @Override
+    public Board readBoard(Path path) {
+        // TODO: Implement reading Board from JSON using Gson's JsonObject/JsonArray
+        return null;
+    }
+}
---
+++ src/main/java/edu/ntnu/iir/bidata/filehandling/board/BoardFileWriterGson.java
@@ -0,0 +1,11 @@
+package edu.ntnu.iir.bidata.filehandling.board;
+
+import edu.ntnu.iir.bidata.model.board.Board;
+import java.nio.file.Path;
+
+public class BoardFileWriterGson implements BoardFileWriter {
+    @Override
+    public void writeBoard(Board board, Path path) {
+        // TODO: Implement writing Board to JSON using Gson's JsonObject/JsonArray
+    }
+}
