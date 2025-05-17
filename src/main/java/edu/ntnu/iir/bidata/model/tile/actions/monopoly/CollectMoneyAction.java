545638de08a8f10931c24fe85ae93739ac9871ab
Kaamya Shinde <kaamyashinde@gmail.com>
2025-05-17 12:22:49 +0200

    feat: add go tile and collect money action

---
+++ src/main/java/edu/ntnu/iir/bidata/model/tile/actions/monopoly/CollectMoneyAction.java
@@ -0,0 +1,34 @@
+package edu.ntnu.iir.bidata.model.tile.actions.monopoly;
+
+import edu.ntnu.iir.bidata.model.tile.core.TileAction;
+import edu.ntnu.iir.bidata.model.player.Player;
+import edu.ntnu.iir.bidata.model.player.SimpleMonopolyPlayer;
+import edu.ntnu.iir.bidata.model.tile.core.Tile;
+
+/**
+ * A tile action that collects money from the player.
+ * 
+ * @author kaamyashinde
+ * @version 1.0
+ */
+public class CollectMoneyAction implements TileAction {
+    /**
+     * Collects money when passing the Go tile.
+     * 
+     * @param player The player that collects the money.
+     * @param currentTile The current tile.
+     */
+    @Override
+    public void executeAction(Player player, Tile currentTile) {
+        SimpleMonopolyPlayer monopolyPlayer = (SimpleMonopolyPlayer) player;
+        monopolyPlayer.collectMoney(200);
+    }
+
+    /**
+     * Gets the description of the action.
+     */
+    @Override
+    public String getDescription() {
+        return "Collect 200 money";
+    }
+}
