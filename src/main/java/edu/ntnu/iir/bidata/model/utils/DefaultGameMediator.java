4d1a1710909ebe42baee560635cfd8690ae534c2
Durva <durvaparmar19@gmail.com>
2025-05-22 16:13:45 +0200

    javadocs for default game mediator class

--- src/main/java/edu/ntnu/iir/bidata/model/utils/DefaultGameMediator.java
+++ src/main/java/edu/ntnu/iir/bidata/model/utils/DefaultGameMediator.java
@@ -4,26 +4,60 @@ import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 import edu.ntnu.iir.bidata.Inject;
 
+/**
+ * Default implementation of the {@link GameMediator} interface.
+ * Maintains a set of listeners and notifies them about events.
+ */
 public class DefaultGameMediator implements GameMediator {
+
     private final Set<GameMediatorListener> listeners = new CopyOnWriteArraySet<>();
 
+    /**
+     * Constructs a new {@code DefaultGameMediator} instance.
+     */
     @Inject
     public DefaultGameMediator() {}
 
+    /**
+     * Registers a new {@link GameMediatorListener} to receive event notifications.
+     *
+     * @param listener the listener to register
+     */
     public void register(GameMediatorListener listener) {
         listeners.add(listener);
     }
 
+    /**
+     * Unregisters a previously registered {@link GameMediatorListener}.
+     *
+     * @param listener the listener to remove
+     */
     public void unregister(GameMediatorListener listener) {
         listeners.remove(listener);
     }
 
+    /**
+     * Notifies all registered listeners of a specific event.
+     *
+     * @param sender the source of the event
+     * @param event the name or description of the event
+     */
     @Override
     public void notify(Object sender, String event) {
         listeners.forEach(listener -> listener.onEvent(sender, event));
     }
 
+    /**
+     * Listener interface for receiving game mediator events.
+     */
     public interface GameMediatorListener {
+
+        /**
+         * Invoked when an event occurs.
+         *
+         * @param sender the source of the event
+         * @param event the name or description of the event
+         */
         void onEvent(Object sender, String event);
     }
-} 
+}
